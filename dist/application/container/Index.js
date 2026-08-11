d103aee7928cff8d73da59f59edafc73ac30f4e2
AdrianMnd <adrian.mend7@gmail.com>
2026-08-11 11:43:07 +0200

    Delete dist directory

--- dist/application/container/Index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.container = void 0;
-require("dotenv/config");
-const ApplicationContainer_1 = require("./ApplicationContainer");
-exports.container = new ApplicationContainer_1.ApplicationContainer();
--- dist/presentation/api/routes/chat.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const Index_1 = require("../../../application/container/Index");
-const chat_controller_1 = require("../controllers/chat.controller");
-const router = (0, express_1.Router)();
-const controller = new chat_controller_1.ChatController(Index_1.container.askQuestionUseCase);
-router.post("/", controller.ask);
-exports.default = router;
